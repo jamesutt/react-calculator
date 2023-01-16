950b9cec466b96516baa2eeb2b1f6f0573e792fc
james <jms.sutt@gmail.com>
2023-01-16 23:46:06 +0700

    add packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.slate,
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/forms')],
+}
