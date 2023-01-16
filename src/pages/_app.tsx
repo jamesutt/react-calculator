4aadb2ab4a2d576f1f144d4d634e324b35b9248b
james <jms.sutt@gmail.com>
2023-01-17 00:30:42 +0700

    add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import { type AppType } from 'next/dist/shared/lib/utils'
+import Head from 'next/head'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-import "../styles/globals.css";
+import '../styles/globals.css'
+
+const queryClient = new QueryClient()
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Title</title>
+        <meta name="description" content="Description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
+}
 
-export default MyApp;
+export default MyApp
